a5d6d7b5f49c99f5be5fdabf25f7df8034da2fe1
François Martineau <francoismartineau@github.com>
2018-04-11 17:05:51 -0400

    avecement du site

--- siteweb/serveur_applicatif.py
+++ siteweb/serveur_applicatif.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 app = Flask(__name__)
 
@@ -9,12 +10,13 @@ app = Flask(__name__)
 
 @app.route("/")                             # vers l'élément racine "/"
 def main():                                 # point d'entrée
-    return render_template("index.html")        # rendu d'une page .html
+    print(request.args.get('idText'))
+    maliste = {'hello' : 10}
+    return render_template("index.html", maliste = maliste)        # rendu d'une page .html
 
 @app.route("/ProchainePage")                # doit concorder avec l'attribut href de la balise qui déclenche la fonction
 def ProchainePag():                         # le nom de fonction n'a pas d'importance
-    var_python = "Python vous salue"
-    return render_template("Page2.html", var_html = var_python)
+    return render_template("Page2.html")
 
 
 if __name__ == "__main__":
