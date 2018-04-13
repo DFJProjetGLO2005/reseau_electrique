c8dd30095761eb62f2d041766acea5b55021222b
François Martineau <francoismartineau@github.com>
2018-04-12 20:40:27 -0400

    Mise à jour readme

--- requetes/listeAbonnes.py
+++ requetes/listeAbonnes.py
@@ -1,7 +1,16 @@
+"""
+    Brief: Cette classe permet de faire les requêtes nécessaires
+           pour afficher la page listeAbonnes.html du siteweb.
+"""
 class ListeAbonnes:
     def __init__(self, req):
         self.execute = req.execute
 
+    """
+        Brief: Cette fonction retourne les informations relative à une liste d'abonnées.
+        Param[in]: (int list) Une liste d'identificateurs d'abonnés.
+        Return: 
+    """
     def get_data(self, aids):
         aids = "({})".format(", ".join(str(a) for a in aids))
         return self.execute("SELECT Aid, Nom, Telephone FROM Abonnes WHERE Aid in {};".format(aids))
